77c2250e30d5c8373851362fcded49495b208c69
Libertas <horizonvei@gmail.com>
2013-04-30 13:00:57 +0800

    bug of install.sh fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 
-cp -r ./ /opt/
-echo "python3 /opt/lcal.py" > /usr/local/bin/lcal
+cp -r ./ /opt/lcal
+echo "python3 /opt/lcal/lcal.py" > /usr/local/bin/lcal
 chmod +x /usr/local/bin/lcal
